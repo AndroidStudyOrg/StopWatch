12e9ec20d02ec6badbcbba7e32b6288795fe776d
wbham <wbham97@naver.com>
2023-10-09 05:16:24 +0900

    [Add] - 다이얼 로그 추가
    - NumberPicker를 포함하는 layout을 새로 생성 후 이를 Dialog안에 넣는 법
    - 새로 생성한 layout에 대해 binding을 설정하고 이를 설정하는 방법
    - StringFormat을 활용하는 방법

--- app/src/main/java/org/shop/stopwatch/MainActivity.kt
+++ app/src/main/java/org/shop/stopwatch/MainActivity.kt
@@ -2,14 +2,95 @@ package org.shop.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import org.shop.stopwatch.databinding.ActivityMainBinding
+import org.shop.stopwatch.databinding.DialogCountdownSettingBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var countDownSec = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
+
+        binding.tvCountDown.setOnClickListener {
+            showCountDownDialog()
+        }
+
+        binding.fabStart.setOnClickListener {
+            start()
+            binding.fabStart.isVisible = false
+            binding.fabStop.isVisible = false
+            binding.fabPause.isVisible = true
+            binding.fabLap.isVisible = true
+        }
+
+        binding.fabStop.setOnClickListener {
+            showAlertDialog()
+        }
+
+        binding.fabPause.setOnClickListener {
+            pause()
+            binding.fabStart.isVisible = true
+            binding.fabStop.isVisible = true
+            binding.fabPause.isVisible = false
+            binding.fabLap.isVisible = false
+        }
+
+        binding.fabLap.setOnClickListener {
+            lap()
+        }
+    }
+
+    private fun start() {
+
+    }
+
+    private fun stop() {
+        binding.fabStart.isVisible = true
+        binding.fabStop.isVisible = true
+        binding.fabPause.isVisible = false
+        binding.fabLap.isVisible = false
+    }
+
+    private fun pause() {
+
+    }
+
+    private fun lap() {
+
+    }
+
+    private fun showCountDownDialog() {
+        AlertDialog.Builder(this).apply {
+            val dialogBinding = DialogCountdownSettingBinding.inflate(layoutInflater).also {
+                with(it.npCountDownSec) {
+                    maxValue = 20
+                    minValue = 0
+                    value = countDownSec
+                }
+                setView(it.root)
+            }
+            setTitle("카운트다운 설정")
+            setPositiveButton("확인") { dialog, id ->
+                countDownSec = dialogBinding.npCountDownSec.value
+                binding.tvCountDown.text = String.format("%02d", countDownSec)
+            }
+            setNegativeButton("취소", null)
+        }.show()
+    }
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("종료하시겠습니까?")
+            setPositiveButton("네") { dialog, id ->
+                stop()
+            }
+            setNegativeButton("아니요", null)
+        }.show()
     }
 }
