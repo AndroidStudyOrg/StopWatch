3d280d320405eff9f57228ee4cbac6cf9e3db075
wbham <wbham97@naver.com>
2023-10-10 05:10:04 +0900

    [Feat] - 타이머 기능 구현
    - 1Tick 단위의 UI 업데이트 및 초, 분 UI 업데이트.
    - timer (WokerThread)에서의 작업을 UI 업데이트 하기 위해 runOnUiThread와 view.post 사용
    - 카운트 다운 입력 초와 ProgressBar와의 연결
    - 카운트 다운이 끝난 후에는 관련 UI 보이지 않게 숨김. 이때 xml에서 Helper의 Group 사용
    - 자세한 내용은 주석 참조

--- app/src/main/java/org/shop/stopwatch/MainActivity.kt
+++ app/src/main/java/org/shop/stopwatch/MainActivity.kt
@@ -6,10 +6,23 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import org.shop.stopwatch.databinding.ActivityMainBinding
 import org.shop.stopwatch.databinding.DialogCountdownSettingBinding
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    // 카운트 다운 시간
     private var countDownSec = 10
+
+    // 진행되는 카운트 다운 시간 (1 Second == 10 DeciSecond)
+    private var currentCountDownDeciSec = countDownSec * 10
+
+    // 올라가는 숫자를 체크하기 위한 변수(0.1초 단위의 숫자)
+    private var currentDeciSecond = 0
+
+    // pause에서 timer를 제어하기 위한 instance 생성
+    private var timer: Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,10 +57,48 @@ class MainActivity : AppCompatActivity() {
         binding.fabLap.setOnClickListener {
             lap()
         }
+
+        initCountDownView()
+    }
+
+    private fun initCountDownView() {
+        binding.tvCountDown.text = String.format("%02d", countDownSec)
+        binding.pbCountDown.progress = 100
     }
 
     private fun start() {
+        timer = timer(initialDelay = 0, period = 100) {
+            if (currentCountDownDeciSec == 0) {
+                currentDeciSecond += 1
+//            Log.d("currentDeciSecond", currentDeciSecond.toString())
+                val minutes = currentDeciSecond.div(10) / 60
+                val second = currentDeciSecond.div(10) % 60
+                val deciSecond = currentDeciSecond % 10
+                /**
+                 * 메인 스레드에서 call 할 수 있는 방법
+                 * 1. runOnUiThread
+                 * 2. View.post
+                 * 3. View.postDelayed
+                 * 4. Handler
+                 */
+                runOnUiThread {
+                    binding.tvTimeText.text = String.format("%02d:%02d", minutes, second)
+                    binding.tvTickText.text = deciSecond.toString()
 
+                    binding.groupCountDown.isVisible = false
+                }
+            } else {
+                currentCountDownDeciSec -= 1
+                val seconds = currentCountDownDeciSec / 10
+                val progress = (currentCountDownDeciSec / (countDownSec * 10f)) * 100
+
+                // 어떠한 view 여도 상관 없음
+                binding.root.post {
+                    binding.tvCountDown.text = String.format("%02d", seconds)
+                    binding.pbCountDown.progress = progress.toInt()
+                }
+            }
+        }
     }
 
     private fun stop() {
@@ -55,10 +106,18 @@ class MainActivity : AppCompatActivity() {
         binding.fabStop.isVisible = true
         binding.fabPause.isVisible = false
         binding.fabLap.isVisible = false
+
+        currentDeciSecond = 0
+        binding.tvTimeText.text = "00:00"
+        binding.tvTickText.text = "0"
+
+        binding.groupCountDown.isVisible = true
+        initCountDownView()
     }
 
     private fun pause() {
-
+        timer?.cancel()
+        timer = null
     }
 
     private fun lap() {
@@ -78,6 +137,7 @@ class MainActivity : AppCompatActivity() {
             setTitle("카운트다운 설정")
             setPositiveButton("확인") { dialog, id ->
                 countDownSec = dialogBinding.npCountDownSec.value
+                currentCountDownDeciSec = countDownSec * 10
                 binding.tvCountDown.text = String.format("%02d", countDownSec)
             }
             setNegativeButton("취소", null)
